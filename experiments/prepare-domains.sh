3ec0902aeadc200476c64c54748204a54e3b46f5
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2018-12-28 09:24:29 -0200

    Fixes for  prepare-domains

--- experiments/prepare-domains.sh
+++ experiments/prepare-domains.sh
@@ -4,7 +4,7 @@ echo "Preparing dataset for $1"
 pushd $1
 for obs in 10 30 50 70 100; do
 	mkdir $obs
-	if [ $obs == 100]; then
+	if [ $obs == 100 ]; then
 		suffix="_full"
 	else
 		suffix="_${obs}_"
