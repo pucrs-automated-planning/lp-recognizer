d6d3159bce2f936e68bbaf3c5d87ed6721af811c
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2019-02-17 10:02:48 -0300

    First bash at generating tables for Ramon.

--- experiments/run-all-experiments.sh
+++ experiments/run-all-experiments.sh
@@ -3,28 +3,27 @@
 declare -a domains=(
 				#"blocks-world-small" 
 				#"intrusion-detection-small"
-					"blocks-world" 
-					"blocks-world-aaai"
-				 	"campus"
-				 	"campus-noisy"
-				 	"depots"
-				 	"driverlog"
-				 	"dwr"
-				# 	"dwr"
-				 	"easy-ipc-grid"
-				 	"easy-ipc-grid-noisy"
-				 	"ferry"
-					# 	"hanoi"
-				 	"intrusion-detection"
-				 	"intrusion-detection-noisy"
-				# 	"kitchen"
-				# 	"kitchen-noisy"
-				 	"logistics"
-				 	"miconic"
-				 	"rovers"
-				 	"satellite"
-					"sokoban"
-					"zeno-travel"
+				"blocks-world" 
+				"blocks-world-aaai"
+			 	"campus"
+			 	"campus-noisy"
+			 	"depots"
+			 	"driverlog"
+			 	"dwr"
+			 	"easy-ipc-grid"
+			 	"easy-ipc-grid-noisy"
+			 	"ferry"
+				# 	"hanoi"
+			 	"intrusion-detection"
+			 	"intrusion-detection-noisy"
+				"kitchen"
+				"kitchen-noisy"
+			 	"logistics"
+			 	"miconic"
+			 	"rovers"
+			 	"satellite"
+				"sokoban"
+				"zeno-travel"
 			)
 pushd ..
 # echo "$domains"
@@ -34,19 +33,20 @@ fi
 
 for domain in "${domains[@]}"; do
 	echo "Running domain ${domain}"
-	python2 run_experiments.py $domain -v 
-	mkdir results/h_value
-	mv *.txt ./results/h_value
-	
-	python2 run_experiments.py $domain -c 
-	mkdir results/h_value_c
-	mv *.txt ./results/h_value_c
-	
-	python2 run_experiments.py $domain -s 
-	mkdir results/soft_c
-	mv *.txt ./results/soft_c
-	
-	python2 run_experiments.py $domain -d
-	mkdir results/diff_h_value_c
-	mv *.txt ./results/diff_h_value_c
+	python2 run_experiments.py $domain -v -c -s -d
+	# python2 run_experiments.py $domain -v
+# 	mkdir results/h_value
+# 	mv *.txt ./results/h_value
+#
+# 	python2 run_experiments.py $domain -c
+# 	mkdir results/h_value_c
+# 	mv *.txt ./results/h_value_c
+#
+# 	python2 run_experiments.py $domain -s
+# 	mkdir results/soft_c
+# 	mv *.txt ./results/soft_c
+#
+# 	python2 run_experiments.py $domain -d
+# 	mkdir results/diff_h_value_c
+# 	mv *.txt ./results/diff_h_value_c
 done
--- run_experiments.py
+++ run_experiments.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
-from plan_recognition import *
+import sys, os, csv, time, math
+from plan_recognition import Hypothesis, LPRecognizerDiffHValueC, LPRecognizerHValue, LPRecognizerHValueC, LPRecognizerSoftC, custom_partition, Program_Options
 
 class Experiment:
     def __init__(self, h_value, h_value_c, soft_c, diff_h_value_c):
@@ -9,10 +10,12 @@ class Experiment:
         self.multi_tie_breaking_spread = 0
         self.multi_correct = 0
         self.multi_spread  = 0
+        self.candidate_goals = 0
         self.h_value = h_value
         self.h_value_c = h_value_c
         self.soft_c = soft_c
         self.diff_h_value_c = diff_h_value_c
+        self.totalTime = 0
 
     def reset(self):
         self.unique_correct = 0        
@@ -20,6 +23,8 @@ class Experiment:
         self.multi_tie_breaking_spread = 0
         self.multi_correct = 0
         self.multi_spread  = 0
+        self.candidate_goals = 0
+        self.totalTime = 0
 
     def run_experiment(self, options):
         if self.h_value:
@@ -30,8 +35,12 @@ class Experiment:
             recognizer = LPRecognizerSoftC(options)                   
         if self.diff_h_value_c:
             recognizer = LPRecognizerDiffHValueC(options) 
-        recognizer.run_recognizer()                   
-                    
+        
+        startTime = time.time()
+        recognizer.run_recognizer()
+        experimentTime = time.time() - startTime
+        self.totalTime += experimentTime
+        
         if recognizer.unique_goal is not None and recognizer.get_real_hypothesis() == recognizer.unique_goal:
             self.unique_correct = self.unique_correct + 1
         if recognizer.get_real_hypothesis() in recognizer.multi_goal_tie_breaking:
@@ -40,36 +49,56 @@ class Experiment:
             self.multi_correct = self.multi_correct + 1  
         self.multi_tie_breaking_spread = self.multi_tie_breaking_spread + len(recognizer.multi_goal_tie_breaking)            
         self.multi_spread = self.multi_spread  + len(recognizer.multi_goal_no_tie_breaking)
+        self.candidate_goals = self.candidate_goals + len(recognizer.hyps)
+    
+    def __repr__(self):
+        return "UC=%d MTBC=%d MTBS=%d MC=%d MS=%d CG=%d"%(self.unique_correct,self.multi_tie_breaking_correct,self.multi_tie_breaking_spread,self.multi_correct,self.multi_spread,self.candidate_goals)
 
 def doExperiments(domainName, observability, h_value, h_value_c, soft_c, diff_h_value_c):
     experiment_time = time.time()
+    totalProblems = 0
 
     file_experiment = open("experiment.txt",'a')
     file_experiment.write(domainName+"\n")
 
     print_text = "obs problems"
     experiments_result = "obs,problems"
-    experiments = []    
-    if h_value:       
+    experiment_names = []
+    experiments_tables = {}
+    experiments_tables_tb = {}
+    experiments = {}
+    if h_value:
         print_text = print_text + " h-value"
-        experiments_result = experiments_result  + ",h-value"        
-        experiments.append(Experiment(True, False, False, False))        
+        experiments_result = experiments_result  + ",h-value"
+        experiment_names.append("h-value")
+        experiments["h-value"] = Experiment(True, False, False, False) 
+        experiments_tables["h-value"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
+        experiments_tables_tb["h-value"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
     if h_value_c:
         print_text = print_text  + " h-value-c"          
-        experiments_result = experiments_result  + ",h-value-c"          
-        experiments.append(Experiment(False, True, False, False))        
+        experiments_result = experiments_result  + ",h-value-c"
+        experiment_names.append("h-value-c")
+        experiments["h-value-c"] = Experiment(False, True, False, False)
+        experiments_tables["h-value-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
+        experiments_tables_tb["h-value-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
     if soft_c:
         print_text = print_text  + " soft-c"     
-        experiments_result = experiments_result  + ",soft-c"     
-        experiments.append(Experiment(False, False, True, False))                
+        experiments_result = experiments_result  + ",soft-c"
+        experiment_names.append("soft-c")
+        experiments["soft-c"] = Experiment(False, False, True, False)
+        experiments_tables["soft-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
+        experiments_tables_tb["soft-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
     if diff_h_value_c:
-        print_text = print_text  + " diff-h-value-c"           
-        experiments_result = experiments_result  + ",diff-h-value-c"           
-        experiments.append(Experiment(False, False, False, True))
+        print_text = print_text  + " diff-h-value-c"
+        experiments_result = experiments_result  + ",diff-h-value-c"
+        experiment_names.append("diff-h-value-c")
+        experiments["diff-h-value-c"] = Experiment(False, False, False, True)
+        experiments_tables["diff-h-value-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
+        experiments_tables_tb["diff-h-value-c"] = "Obs  Accuracy  Precision  Recall  F1score  Fallout  Missrate  AvgRecG Total Time\n"
     print_text = print_text + "\n"
     experiments_result = experiments_result + "\n"
 
-    if h_value:       
+    if h_value:
         experiments_result = experiments_result + ",,U,UA,MY,MYA,MYS,MN,MNA,MNS"    
     if h_value_c:
         experiments_result = experiments_result + ",U,UA,MY,MYA,MYS,MN,MNA,MNS"    
@@ -83,8 +112,8 @@ def doExperiments(domainName, observability, h_value, h_value_c, soft_c, diff_h_
 
     for obs in observability:
         problems = 0   
-        for experiment in experiments:
-            experiment.reset()       
+        for e in experiment_names:
+            experiments[e].reset()       
         
         problems_path = 'experiments/' + domainName + '/' + obs + '/'
         for problem_file in os.listdir(problems_path):
@@ -92,7 +121,7 @@ def doExperiments(domainName, observability, h_value, h_value_c, soft_c, diff_h_
                 cmd_clean = 'rm -rf *.pddl *.dat *.log'
                 os.system(cmd_clean)
 
-                print problems_path + problem_file
+                print(problems_path + problem_file)
                 cmd_untar = 'tar xvjf ' + problems_path + problem_file
                 os.system(cmd_untar)
 
@@ -101,22 +130,78 @@ def doExperiments(domainName, observability, h_value, h_value_c, soft_c, diff_h_
                 args = ['-e', problems_path + problem_file]
 
                 options = Program_Options(args)
-                for experiment in experiments:
-                    experiment.run_experiment(options)                         
+                for e in experiment_names:
+                    experiments[e].run_experiment(options)
+                    # print("%s %r"%(e,experiments[e]))
 
         print_text_result = "%s %d "%(obs,problems)             
         result = "%s,%d"%(obs,problems) 
-        for experiment in experiments:
-            print_text_result = print_text_result + "%d "%(experiment.unique_correct)   
+        totalProblems += problems
+        for e in experiment_names:
+            print_text_result = print_text_result + "%d "%(experiments[e].unique_correct)   
             
-            result = result + ",%2.4f"%(experiment.unique_correct)    
-            result = result + ",%2.4f"%(float(experiment.unique_correct)/float(problems))                
-            result = result + ",%2.4f"%(experiment.multi_tie_breaking_correct)    
-            result = result + ",%2.4f"%(float(experiment.multi_tie_breaking_correct)/float(problems))                
-            result = result + ",%2.4f"%(float(experiment.multi_tie_breaking_spread)/float(problems))    
-            result = result + ",%2.4f"%(experiment.multi_correct)    
-            result = result + ",%2.4f"%(float(experiment.multi_correct)/float(problems))                
-            result = result + ",%2.4f"%(float(experiment.multi_spread)/float(problems))                             
+            result = result + ",%2.4f"%(experiments[e].unique_correct)    
+            result = result + ",%2.4f"%(float(experiments[e].unique_correct)/float(problems))                
+            result = result + ",%2.4f"%(experiments[e].multi_tie_breaking_correct)    
+            result = result + ",%2.4f"%(float(experiments[e].multi_tie_breaking_correct)/float(problems)) 
+            result = result + ",%2.4f"%(float(experiments[e].multi_tie_breaking_spread)/float(problems))
+            result = result + ",%2.4f"%(experiments[e].multi_correct)
+            result = result + ",%2.4f"%(float(experiments[e].multi_correct)/float(problems))                
+            result = result + ",%2.4f"%(float(experiments[e].multi_spread)/float(problems))
+
+            experiments_tables[e] += "%s "%(obs)
+            experiments_tables_tb[e] += "%s "%(obs)
+
+            truePositives = float(experiments[e].multi_correct)
+            trueNegatives = float(experiments[e].candidate_goals - experiments[e].multi_spread)
+            falsePositives = float(experiments[e].multi_spread - experiments[e].multi_correct)
+            falseNegatives = float(experiments[e].candidate_goals - experiments[e].multi_correct)
+            # print("TP=%2.4f TN=%2.4f FP=%2.4f FN=%2.4f"%(truePositives,trueNegatives,falsePositives,falseNegatives))
+
+            accuracy = float(experiments[e].multi_correct)/float(problems)
+            precision = truePositives/float(experiments[e].multi_spread) if experiments[e].multi_spread != 0 else 0
+            recall = truePositives/float(problems)
+            if ((precision + recall) == 0):
+                f1score = 0
+            else:
+                f1score = 2 * ((precision * recall) / (precision + recall))
+            fallout = falsePositives / (trueNegatives + falsePositives)
+            missrate = falseNegatives / (falseNegatives + truePositives)
+            experiments_tables[e] += "%2.4f "%(accuracy)
+            experiments_tables[e] += "%2.4f "%(precision)
+            experiments_tables[e] += "%2.4f "%(recall)
+            experiments_tables[e] += "%2.4f "%(f1score)
+            experiments_tables[e] += "%2.4f "%(fallout)
+            experiments_tables[e] += "%2.4f "%(missrate)
+            experiments_tables[e] += "%2.4f "%(float(experiments[e].multi_spread)/float(problems))
+            experiments_tables[e] += "%2.4f "%(float(experiments[e].totalTime)/float(problems))
+            experiments_tables[e] += "\n"
+
+            truePositives = float(experiments[e].multi_tie_breaking_correct)
+            trueNegatives = float(experiments[e].candidate_goals - experiments[e].multi_tie_breaking_spread)
+            falsePositives = float(experiments[e].multi_tie_breaking_spread - experiments[e].multi_tie_breaking_correct)
+            falseNegatives = float(experiments[e].candidate_goals - experiments[e].multi_tie_breaking_correct)
+            # print("TP=%2.4f TN=%2.4f FP=%2.4f FN=%2.4f"%(truePositives,trueNegatives,falsePositives,falseNegatives))
+
+            accuracy = float(experiments[e].multi_tie_breaking_correct)/float(problems)
+            precision = truePositives/float(experiments[e].multi_tie_breaking_spread) if experiments[e].multi_tie_breaking_spread != 0 else 0
+            recall = truePositives/float(problems)
+            if ((precision + recall) == 0):
+                f1score = 0
+            else:
+                f1score = 2 * ((precision * recall) / (precision + recall))
+            fallout = falsePositives / (trueNegatives + falsePositives)
+            missrate = falseNegatives / (falseNegatives + truePositives)
+            experiments_tables_tb[e] += "%2.4f "%(accuracy)
+            experiments_tables_tb[e] += "%2.4f "%(precision)
+            experiments_tables_tb[e] += "%2.4f "%(recall)
+            experiments_tables_tb[e] += "%2.4f "%(f1score)
+            experiments_tables_tb[e] += "%2.4f "%(fallout)
+            experiments_tables_tb[e] += "%2.4f "%(missrate)
+            experiments_tables_tb[e] += "%2.4f "%(float(experiments[e].multi_tie_breaking_spread)/float(problems))
+            experiments_tables_tb[e] += "%2.4f "%(float(experiments[e].totalTime)/float(problems))
+            experiments_tables_tb[e] += "\n"
+
 
         print_text_result = print_text_result + "\n"
         result = result + "\n"    
@@ -129,10 +214,22 @@ def doExperiments(domainName, observability, h_value, h_value_c, soft_c, diff_h_
         print(print_text)
         file_result.write(experiments_result)
         file_result.close()
+    
+    for e in experiment_names:
+        experiments_tables[e] += '\n$> Total Problems: ' + str(totalProblems)
+        experiments_tables_tb[e] += '\n$> Total Problems: ' + str(totalProblems)
+        table_file = open(str(domainName) + "-" + e +'.txt', 'w')
+        table_file_tb = open(str(domainName) + "-" + e +'-tb.txt', 'w')
+        table_file.write(experiments_tables[e])
+        table_file_tb.write(experiments_tables_tb[e])
+        table_file.close()
+        table_file_tb.close()
+
     file_experiment.write(experiments_result)
     final_time = time.time() - experiment_time
     file_experiment.close()
     print('Experiment Time: {0:3f}'.format(final_time))
+
 
 def main():
     domainName = sys.argv[1]
