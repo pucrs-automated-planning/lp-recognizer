9de3ea83d3141f217242032492557aaed2bb43bd
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2019-08-30 20:28:31 -0300

    Cleaned up code to run in multiple screens.

--- experiments/run-all-experiments-screens.sh
+++ experiments/run-all-experiments-screens.sh
@@ -72,28 +72,9 @@ for domain in "${domains[@]}"; do
 	echo "Running domain ${domain} at $folder"
 	screen -dmS lp-recognizer-$domain zsh -c "python2 run_experiments.py $domain -s -d -u -n -f -k; mv *.txt ..; cd ..; rm -rf lp-recognizer-$domain"
 	popd
-	# python2 run_experiments.py $domain -v -c -s -d -u -n
-	# python2 run_experiments.py $domain -v
-	# mkdir results/h_value
-# 	mv *.txt ./results/h_value
-#
-# 	python2 run_experiments.py $domain -c
-# 	mkdir results/h_value_c
-# 	mv *.txt ./results/h_value_c
-#
-# 	python2 run_experiments.py $domain -s
-# 	mkdir results/soft_c
-# 	mv *.txt ./results/soft_c
-#
-# 	python2 run_experiments.py $domain -d
-# 	mkdir results/delta_h_c
-# 	mv *.txt ./results/delta_h_c
 done
 
 for domain in "${noisy_domains[@]}"; do
-	# echo "Running domain ${domain}"
-# 	python2 run_experiments.py $domain -s -d -u -n -f -k
-# 	mv *.txt results
 	echo "Copying domain ${domain}"
 	cp -R ../lp-recognizer ../clones/lp-recognizer-$domain
 	pushd ../clones/lp-recognizer-$domain
